deab97ac3874259296460c55483b5a932477651e
ilhamsyamsuddin <32873409+ilhamsyamsuddin@users.noreply.github.com>
2020-01-13 13:49:42 +0700

    Create hckr_armygame.cpp
    https://www.hackerrank.com/challenges/game-with-cells/problem

---
+++ hckr_armygame.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define pf printf
+#define sf scanf 
+#define z long long int 
+using namespace std ; 
+int main() { 	 	
+    z a,b; 	sf("%lld %lld",&b,&a); 	
+    pf("%lld",( ((b+1)/2) *((a+1)/2) ) ); 	
+    return 0; 
+    } 
+
